e3bcb303a07d6c9a89ddf1caa37be2693a6823b3
Muhammad Sulaiman <trendingofyt@gmail.com>
2022-09-05 19:59:58 +0500

    Done

--- src/Components/FirstCard.js
+++ src/Components/FirstCard.js
@@ -10,7 +10,7 @@ const FirstCard = (props) => {
                     <div className="flex items-center gap-2">
                     <div className="rounded-full bg-white border w-[50px] h-[30px]"></div>
                     <div className="rounded-full bg-white border w-[50px] h-[30px]"></div>
-                    <div className={`rounded-full bg-[${props.enteringColor}] border w-[50px] h-[30px]`}></div>
+                    <div className={`rounded-full border w-[50px] h-[30px]`} style={{background: props.enteringColor}}></div>
                     </div>
                   </div>
 
@@ -18,7 +18,7 @@ const FirstCard = (props) => {
                     <div className="text-[19px] text-gray-900 font-[400]">BNBUSDT</div>
                     <div className="flex items-center gap-2">
                     <div className="rounded-full bg-white border w-[50px] h-[30px]"></div>
-                    <div className={`rounded-full bg-[${props.closeTo}] border w-[50px] h-[30px]`}></div>
+                    <div className={`rounded-full border w-[50px] h-[30px]`} style={{background: props.closeTo}}></div>
                     <div className="rounded-full bg-white border w-[50px] h-[30px]"></div>
                     </div>
                   </div>
@@ -30,7 +30,7 @@ const FirstCard = (props) => {
                     <div className="text-[19px] text-gray-900 font-[400]">BTCUSDT</div>
                     <div className="flex items-center gap-2">
                     <div className="rounded-full bg-white border w-[50px] h-[30px]"></div>
-                    <div className={`rounded-full bg-[${props.closeTo}] border w-[50px] h-[30px]`}></div>
+                    <div className={`rounded-full border w-[50px] h-[30px]`} style={{background: props.closeTo}}></div>
                     <div className="rounded-full bg-white border w-[50px] h-[30px]"></div>
                     </div>
                   </div>
@@ -38,7 +38,7 @@ const FirstCard = (props) => {
                   <div className="flex items-center gap-5">
                     <div className="text-[19px] text-gray-900 font-[400]">XRPUSDT</div>
                     <div className="flex items-center gap-2">
-                    <div className={`rounded-full bg-[${props.out}] border w-[50px] h-[30px]`}></div>
+                    <div className={`rounded-full border w-[50px] h-[30px]`} style={{background: props.out}}></div>
                     <div className="rounded-full bg-white border w-[50px] h-[30px]"></div>
                     <div className="rounded-full bg-white border w-[50px] h-[30px]"></div>
                     </div>
@@ -48,17 +48,17 @@ const FirstCard = (props) => {
                <div className="flex items-center justify-center mt-14 gap-[50px]">
                <div className="flex items-center gap-5">
                <div className="text-[19px] text-gray-900 font-[400]">Entering</div>
-               <div className={`rounded-full bg-[${props.enteringColor}] border w-[45px] h-[30px]`}></div>
+               <div className={`rounded-full border w-[45px] h-[30px]`} style={{background: props.enteringColor}}></div>
                </div>
 
                <div className="flex items-center gap-5">
                <div className="text-[19px] text-gray-900 font-[400]">Close to</div>
-               <div className={`rounded-full bg-[${props.closeTo}] border w-[45px] h-[30px]`}></div>
+               <div className={`rounded-full border w-[45px] h-[30px]`} style={{background: props.closeTo}}></div>
                </div>
 
                <div className="flex items-center gap-5">
                <div className="text-[19px] text-gray-900 font-[400]">Out</div>
-               <div className={`rounded-full bg-[${props.out}] border w-[45px] h-[30px]`}></div>
+               <div className={`rounded-full border w-[45px] h-[30px]`} style={{background: props.out}}></div>
                </div>
                 
                </div>
